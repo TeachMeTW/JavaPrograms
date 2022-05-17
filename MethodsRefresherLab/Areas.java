226fdc51fcf25e40915857d6cafd283339e0e8ef
Robin <teachmetheways652@gmail.com>
2022-05-16 22:49:03 -0700

    Added comments to MethodsRefresherLab

--- MethodsRefresherLab/Areas.java
+++ MethodsRefresherLab/Areas.java
@@ -1,18 +1,23 @@
 package MethodsRefresherLab;
 
 public class Areas {
+    // return double of the circle area from an inputted radius
     public static double areaCircle(double radius) {
         return Math.PI*(Math.pow(radius,2));
     }
+    // return double of the square area from an inputted side
     public static double areaSquare(double side) {
         return Math.pow(side, 2);
     }
+    // return double of the triangle area from a base and height
     public static double areaTriangle(double base, double height) {
         return (base * height)/2;
     }
+    // return double of the rectangle area from a base and height
     public static double areaRectangle(double base, double height) {
         return base * height;
     }
+    // return double of the trapezoid area from a top length, bottom length, and height
     public static double areaTrapezoid(double top, double bottom, double height) {
         return ((top+bottom)*(height/2));
     }
--- MethodsRefresherLab/CalculateAreas.java
+++ MethodsRefresherLab/CalculateAreas.java
@@ -3,56 +3,95 @@ package MethodsRefresherLab;
 import java.util.Scanner;
 
 public class CalculateAreas {
-    public static void main(String args[]) {
-        userChoice();
-    }
+    // create a scanner object in order to get user input
+    public static Scanner userScanner = new Scanner(System.in);
+
+    // Circle Scenario
     public static void runCircle() {
-        Scanner userScanner = new Scanner(System.in);
-        System.out.println("Enter radius");
+        // prompts user to input radius
+        System.out.println("\n\nEnter radius of circle: ");
+        // stores input - variable init
         Double userInput = userScanner.nextDouble();
-        System.out.printf("Circle area:  %.2f", Areas.areaCircle(userInput));
-        userScanner.close();
+        // runs the area calculation from areas class passing in user input
+        System.out.printf("\nCircle area:  %.2f", Areas.areaCircle(userInput));
     }
+
+    // Square scenario
     public static void runSquare() {
-        Scanner userScanner = new Scanner(System.in);
-        userScanner.close();
+        // prompts user to input side of square
+        System.out.println("\n\nEnter side of square: ");
+        // stores input - variable init
+        Double userInput = userScanner.nextDouble();
+        // runs the area calculation from areas class pasing in user input
+        System.out.printf("\nSquare area:  %.2f", Areas.areaSquare(userInput));
     }
+
+    // Triangle Scenario
     public static void runTriangle() {
-        Scanner userScanner = new Scanner(System.in);
-        userScanner.close();
+        // prompts user to input base
+        System.out.println("\n\nEnter base of triangle: ");
+        // stores input - variable init
+        Double userBase = userScanner.nextDouble();
+        // prompts user to input height
+        System.out.println("Enter height of the triange: ");
+        // stores input - variable init
+        Double userHeight = userScanner.nextDouble();
+        // runs the area calculation from areas class pasing in user input
+        System.out.printf("\nTriange area:  %.2f", Areas.areaTriangle(userBase, userHeight));
     }
+
+    // Rectangle Scenario
     public static void runRectangle() {
-        Scanner userScanner = new Scanner(System.in);
-        userScanner.close();
+        // prompts user to input base
+        System.out.println("\n\nEnter base of rectangle: ");
+        // stores input - variable init
+        Double userBase = userScanner.nextDouble();
+        // prompts user to input height
+        System.out.println("Enter height of rectangle: ");
+        // stores input - variable init
+        Double userHeight = userScanner.nextDouble();
+        // runs the area calculation from areas class pasing in user input
+        System.out.printf("\nRectangle area:  %.2f", Areas.areaRectangle(userBase, userHeight));
     }
+
+    // Trapezoid Scenario
     public static void runTrapezoid() {
-        Scanner userScanner = new Scanner(System.in);
-        userScanner.close();
+        // prompts user to input top length
+        System.out.println("\n\nEnter top length of trapezoid: ");
+        // stores input - variable init
+        Double userTop = userScanner.nextDouble();
+        // prompts user to input bottom length
+        System.out.println("Enter bottom length of trapezoid: ");
+        // stores input - variable init
+        Double userBottom = userScanner.nextDouble();
+        // prompts user to input height
+        System.out.println("Enter height of trapezoid: ");
+        // stores input - variable init
+        Double userHeight = userScanner.nextDouble();
+        // runs the area calculation from areas class pasing in user input
+        System.out.printf("\nTrapezoid area:  %.2f   ", Areas.areaTrapezoid(userTop, userBottom, userHeight));
     }
+
+    // User prompt
     public static void userChoice() {
-        Scanner userScanner = new Scanner(System.in);
-        System.out.println("Enter what area you want? Circle, Square, Triangle, Rectangle, Trapezoid, all");
+        System.out.println("\nEnter what area you want? Circle, Square, Triangle, Rectangle, Trapezoid, all\n");
         String userInput = userScanner.nextLine();
+        // Switch statement to determine user option, toLower in order to adapt any cases
         switch(userInput.toLowerCase()) {
             case "circle":
                 runCircle();
-                userScanner.close();
                 break;
             case "square":
                 runSquare();
-                userScanner.close();
                 break;
             case "triangle":
                 runTriangle();
-                userScanner.close();
                 break;
             case "rectangle":
                 runRectangle();
-                userScanner.close();
                 break;
             case "trapezoid":
                 runTrapezoid();
-                userScanner.close();
                 break;
             case "all":
                 runCircle();
@@ -60,11 +99,18 @@ public class CalculateAreas {
                 runTrapezoid();
                 runTriangle();
                 runRectangle();
-                userScanner.close();
                 break;
+            // Default case if user input was invalid, recursively run again until valid
             default:
                 System.out.println("Enter valid input");
                 userChoice();
         }
+    }
+    // main method
+    public static void main(String args[]) {
+        // run user prompt
+        userChoice();
+        // close scanner so no memory leak
+        userScanner.close();
     }
 }
