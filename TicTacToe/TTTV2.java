544a893650d74f683f7bf5672e97258b41196606
Robin <teachmetheways652@gmail.com>
2022-06-08 17:09:39 -0700

    FINALLY!

--- TicTacToe/TTTV2.java
+++ TicTacToe/TTTV2.java
@@ -17,7 +17,7 @@ public class TTTV2 extends JFrame {
     boolean[] players = new boolean[col_row];
     boolean gameEnd = false;
     int count = 0;
-    int seq = 0;
+
 
     Map<String, Integer> pairV = new HashMap<String, Integer>();
     Map<String, Integer> pairH = new HashMap<String, Integer>();
@@ -110,23 +110,9 @@ public class TTTV2 extends JFrame {
     public void checkCondition() {
 
         // Check for horiz
-        for (int k = 1; k<col_row+1;k++) {
-            for(int i = (k-1)*col_row, j = i+1;  j < k*col_row && i < k*col_row; i++, j++) {
-                //System.out.println("Current b1: " + buttons[i].getText() + " Current b2: " + buttons[j].getText() + " char: " + pIcon[z]);
-                if(buttons[i].getText() == buttons[j].getText() && (buttons[i].getText() != "" && buttons[j].getText() != "")) {
-                    seq++;
-                    //System.out.println("\n Matching " + seq);
-                }
-                if (seq == Player.winCon-1) {
-                    System.out.println("H");
-                    Won(buttons[i].getText());
-                    break;
-                    
-                }
-            }
-            seq = 0;
-        }
+
         // check for vert
+        checkHoriz(0);
         checkVert(0);
         checkDiag(0);
 
@@ -150,8 +136,6 @@ public class TTTV2 extends JFrame {
         //     }
         
         // }
-        seq = 0;
-
         for (int i = 0; i < size; i++) {
             if(buttons[i].getText() != "") {
                 count++;
@@ -164,6 +148,30 @@ public class TTTV2 extends JFrame {
 
     }
 
+    public void checkHoriz(int start) {
+        for (int k = 1; k<col_row+1;k++) {
+            for(int i = (k-1)*col_row, j = i+1;  j < k*col_row && i < k*col_row; i++, j++) {
+                //System.out.println("Current b1: " + buttons[i].getText() + " Current b2: " + buttons[j].getText() + " char: " + pIcon[z]);
+                if(buttons[i].getText() == buttons[j].getText() && (buttons[i].getText() != "" && buttons[j].getText() != "")) {
+                    pairH.put(buttons[j].getText(), pairH.get(buttons[j].getText())+1);
+                    for (Map.Entry <String, Integer> entry: pairH.entrySet()) {
+                        if (entry.getValue() == Player.winCon-1) {
+                            System.out.println("H");
+                            Won(entry.getKey());
+                            break;
+                            }
+                        }
+                    //System.out.println("\n Matching " + seq);
+                }
+
+                }
+            for (int i = 0; i < Player.playercount; i++) {
+                pairH.put(pIcon[i], 0);
+            }
+        }
+    }
+
+
     // NEED TO GET A CHECK ON THE VALUE WITHOUT CHANGING TEXT!
     // maybe create new hash
     public void checkVert(int start) {
@@ -173,16 +181,17 @@ public class TTTV2 extends JFrame {
             if(buttons[i].getText() == buttons[j].getText() && (buttons[i].getText() != "" && buttons[j].getText() != "")) {
                 pairV.put(buttons[j].getText(), pairV.get(buttons[j].getText())+1);
                 // buttons[i].setText(buttons[j].getText()+" ");
+                for (Map.Entry <String, Integer> entry: pairV.entrySet()) {
+                    if (entry.getValue() == Player.winCon-1) {
+                        System.out.println("V");
+                        Won(entry.getKey());
+                        break;
+                    }
+                }
                 checkVert(j);
                 // System.out.println("\n Matching " + seq);
             }
-            for (Map.Entry <String, Integer> entry: pairV.entrySet()) {
-                if (entry.getValue() == Player.winCon-1) {
-                    System.out.println("V");
-                    Won(entry.getKey());
-                    break;
-                }
-            }
+
         }
         for (int i = 0; i < Player.playercount; i++) {
             pairV.put(pIcon[i], 0);
@@ -198,34 +207,40 @@ public class TTTV2 extends JFrame {
                 if((buttons[i].getText() == buttons[k].getText() && (buttons[i].getText() != "" && buttons[k].getText() != ""))) {
                     pairDL.put(buttons[k].getText(), pairDL.get(buttons[k].getText())+1);
                     //buttons[i].setText(buttons[k].getText()+" ");
+                    for (Map.Entry <String, Integer> entry: pairDL.entrySet()) {
+                        if (entry.getValue() == Player.winCon-1) {
+                            System.out.println("DL");
+                            Won(entry.getKey());
+                            break;
+                        }
+                    }
                     checkDiag(k);
                     //System.out.println("\n Matching " + seq);
                 }
-                for (Map.Entry <String, Integer> entry: pairDL.entrySet()) {
-                    if (entry.getValue() == Player.winCon-1) {
-                        System.out.println("DL");
-                        Won(entry.getKey());
-                        break;
-                    }
-                }
+
             }
             if((buttons[i].getText() == buttons[j].getText() && (buttons[i].getText() != "" && buttons[j].getText() != ""))) {
                 pairDR.put(buttons[j].getText(), pairDR.get(buttons[j].getText())+1);
                 // buttons[i].setText(buttons[j].getText()+" ");
+                for (Map.Entry <String, Integer> entry: pairDR.entrySet()) {
+                    if (entry.getValue() == Player.winCon-1) {
+                        System.out.println("DR");
+                        Won(entry.getKey());
+                        break;
+                    }
+                }
                 checkDiag(j);
                 // System.out.println("\n Matching " + seq);
             }
-            for (Map.Entry <String, Integer> entry: pairDR.entrySet()) {
-                if (entry.getValue() == Player.winCon-1) {
-                    System.out.println("DR");
-                    Won(entry.getKey());
-                    break;
-                }
-            }
+
 
 
             // System.out.println("Current b1: " + buttons[i].getText() + " Current b2: " + buttons[j].getText());
 
+        }
+        for (int i = 0; i < Player.playercount; i++) {
+            pairDR.put(pIcon[i], 0);
+            pairDL.put(pIcon[i], 0);
         }
     }
 
